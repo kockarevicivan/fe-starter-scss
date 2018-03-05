fcecd7bd69ccdae34cad4ca44fab16bb0ec64c1d
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2018-03-05 13:54:55 +0100

    Added minify and concat task.

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat');
+let minify = require('gulp-minify-css');
 
 gulp.task('default', function() {
 	return gulp.src('scss/**/*.scss')
-		.pipe(sass()) // Converts Sass to CSS with gulp-sass
+		.pipe(sass())
+		.pipe(concat('style.css'))
+		.pipe(minify())
 		.pipe(gulp.dest('assets/css'))
 });
 
